8a1c23ba9cc228689218849182225f4a7876351d
Célestin <celestin.moenne@gmail.com>
2022-03-22 21:09:47 -0400

    start using database

--- app/src/main/java/com/example/schlouky/GameActivity.java
+++ app/src/main/java/com/example/schlouky/GameActivity.java
@@ -7,21 +7,18 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 import java.util.ArrayList;
 
 public class GameActivity extends AppCompatActivity {
 
-    ArrayList<String> questions = new ArrayList<String>() {
-        {
-            add("hey slt comment ça ? Tu fais quoi aujourd'hui ? Oh cool trop bien j'adore ta vie");
-            add("slt");
-            add("yo");
-        }
-    };
+    final int questionNb = 5;
+    int playerNb = 5;
+    int notDrinkingPlayerNb = 1;
+    ArrayList<String> questions = new ArrayList<String>();
 
     int currentQuestionIndex = 0;
-
-    int questionNb;
 
     float threshold = 300;
     float x1, x2, y1, y2, dx, dy;
@@ -33,7 +30,7 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        questionNb = questions.size();
+        LoadQuestions();
 
         Bundle bundle = new Bundle();
         bundle.putString("question", questions.get(currentQuestionIndex));
@@ -46,32 +43,31 @@ public class GameActivity extends AppCompatActivity {
         findViewById(R.id.question_location).setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                switch(event.getAction()) {
-                    case(MotionEvent.ACTION_DOWN): {
+                switch (event.getAction()) {
+                    case (MotionEvent.ACTION_DOWN): {
                         x1 = event.getX();
                         y1 = event.getY();
                         return true;
                     }
-                    case(MotionEvent.ACTION_UP): {
+                    case (MotionEvent.ACTION_UP): {
                         x2 = event.getX();
                         y2 = event.getY();
-                        dx = x2-x1;
-                        dy = y2-y1;
+                        dx = x2 - x1;
+                        dy = y2 - y1;
 
                         // Use dx and dy to determine the direction of the move
-                        if(Math.abs(dx) > Math.abs(dy)) {
+                        if (Math.abs(dx) > Math.abs(dy)) {
                             if (Math.abs(dx) > threshold) {
-                                if(dx<0) {
+                                if (dx < 0) {
                                     //Next question
                                     currentQuestionIndex++;
-                                    if(currentQuestionIndex == questionNb){
+                                    if (currentQuestionIndex == questionNb) {
                                         getSupportFragmentManager().beginTransaction()
                                                 .setCustomAnimations(R.anim.slide_in, R.anim.fade_out, R.anim.fade_in, R.anim.slide_out)
                                                 .setReorderingAllowed(true)
                                                 .add(R.id.question_location, EndGameFragment.class, null)
                                                 .commit();
-                                    }
-                                    else if (currentQuestionIndex < questionNb) {
+                                    } else if (currentQuestionIndex < questionNb) {
                                         Bundle bundle = new Bundle();
                                         bundle.putString("question", questions.get(currentQuestionIndex));
 
@@ -80,12 +76,10 @@ public class GameActivity extends AppCompatActivity {
                                                 .setReorderingAllowed(true)
                                                 .add(R.id.question_location, QuestionFragment.class, bundle)
                                                 .commit();
-                                    }
-                                    else {
+                                    } else {
                                         currentQuestionIndex = questionNb;
                                     }
-                                }
-                                else {
+                                } else {
                                     //Previous question
                                     currentQuestionIndex--;
                                     if (currentQuestionIndex >= 0) {
@@ -97,8 +91,7 @@ public class GameActivity extends AppCompatActivity {
                                                 .setReorderingAllowed(true)
                                                 .add(R.id.question_location, QuestionFragment.class, bundle)
                                                 .commit();
-                                    }
-                                    else {
+                                    } else {
                                         currentQuestionIndex = 0;
                                     }
                                 }
@@ -116,5 +109,30 @@ public class GameActivity extends AppCompatActivity {
     public void StartSetupActivity(View view) {
         Intent intent = new Intent(this, SetupActivity.class);
         startActivity(intent);
+    }
+
+    private void LoadQuestions() {
+        DatabaseManager db = DatabaseManager.getInstance(this);
+        db.setupDatabase();
+
+        db.addPlayer("Stéphane", true);
+        db.addPlayer("Robin", false);
+        db.addPlayer("Célestin", true);
+        db.addPlayer("Eva", true);
+        db.addPlayer("Mathieu", true);
+
+        //TODO: changer le format des questions pour faciliter le remplacer des noms et nombres de gorgées
+        //par exemple utiliser {player1} et {schloukCount}
+
+        db.addQuestion("%s doit mettre une grosse droite à %s.", 2, 0);
+        db.addQuestion("%s choisi la plus magnifique entre %s et %s. Le gagnant boit 2 gorgées.", 3, 2);
+        db.addQuestion("%s ne doit pas oublier le petit bonhomme.", 1, 1);
+        db.addQuestion("Duel de regards entre %s et %s. Le perdant boit 4 gorgées.", 2, 2);
+        db.addQuestion("Duel de regards entre %s et %s.", 2, 0);
+
+        for (int i = 0; i < questionNb; i++) {
+            questions.add(db.getQuestion(ThreadLocalRandom.current().nextInt(0, playerNb + 1),
+                    ThreadLocalRandom.current().nextInt(0, notDrinkingPlayerNb + 1)));
+        }
     }
 }
--- app/src/main/java/com/example/schlouky/MainActivity.java
+++ app/src/main/java/com/example/schlouky/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.schlouky;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,27 +17,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DatabaseManager db = DatabaseManager.getInstance(this);
-        db.setupDatabase();
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                // Actions to do after 10 seconds
+                StartNextActivity();
+            }
+        }, 2000);
+    }
 
-        db.addPlayer("Stéphane", true);
-        db.addPlayer("Robin", false);
-        db.addPlayer("Célestin", true);
-        db.addPlayer("Eva", true);
-        db.addPlayer("Mathieu", true);
-
-        //TODO: changer le format des questions pour faciliter le remplacer des noms et nombres de gorgées
-        //par exemple utiliser {player1} et {schloukCount}
-
-        db.addQuestion("%s doit mettre une grosse droite à %s.", 2, 0);
-        db.addQuestion("%s choisi la plus magnifique entre %s et %s. Le gagnant boit 2 gorgées.", 3, 2);
-        db.addQuestion("%s ne doit pas oublier le petit bonhomme.", 1, 1);
-        db.addQuestion("Duel de regards entre %s et %s. Le perdant boit 4 gorgées.", 2, 2);
-        db.addQuestion("Duel de regards entre %s et %s.", 2, 0);
-
-        Toast.makeText(this, db.getQuestion(3, 2), Toast.LENGTH_LONG).show();
-        Toast.makeText(this, db.getQuestion(1, 3), Toast.LENGTH_LONG).show();
-        Toast.makeText(this, db.getQuestion(2, 0), Toast.LENGTH_LONG).show();
-        Toast.makeText(this, db.getQuestion(2, 2), Toast.LENGTH_LONG).show();
+    private void StartNextActivity() {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
     }
 }
