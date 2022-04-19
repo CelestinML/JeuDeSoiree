e13aab7310e94b433b866a798c5072e711a76725
Eva Minot <eva.minot@hotmail.fr>
2022-04-19 14:34:00 -0400

    random background main

--- app/src/main/java/com/example/schlouky/MainActivity.java
+++ app/src/main/java/com/example/schlouky/MainActivity.java
@@ -5,20 +5,26 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
+    int[] backgrounds = new int[]{R.drawable.background1, R.drawable.background2, R.drawable.background3};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        RelativeLayout relativeLayout = findViewById(R.id.RelativeLayout01);
+        relativeLayout.setBackground(getDrawable(backgrounds[new Random().nextInt(backgrounds.length)]));
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
